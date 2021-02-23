995b77586ed0b70af7a1f94cd39a70c5b9181a5c
Katarina Dordevic <kejo79@gmail.com>
2021-02-23 16:39:30 +0100

    work in progress 3

--- src/Select.js
+++ src/Select.js
@@ -1,17 +1,24 @@
 import menu from "./Menu";
+import { useRef } from "react";
 
 function Select() {
+    const select = useRef()
+    function closeBtn(){
+        select.current.style.display = "none";
+    }
+
+
     let day = ''
     for(let dayMenu of menu){
         day += `<option value="">${dayMenu.date}</option>`
          
     }
 return (
-  <div className="select">
+  <div className="select" ref={select}>
     <select name="" id="">
       {day}
     </select>
-    <button>j'en veux encore</button>
+    <button onClick={closeBtn}>j'en veux encore</button>
   </div>
 );
 }
